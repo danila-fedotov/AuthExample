2a9fb59bea9a6664f07f676869087f1fe08a338e
Danila Fedotov <danila.fedotov.v@gmail.com>
2021-12-14 09:20:21 +0300

    The First

---
+++ AuthExample.Fias/IFiasService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AuthExample.Fias
+{
+    public interface IFiasService
+    {
+        [Authorize(Policy = "FIAS_GET_PACKAGES")]
+
+        Task<(List<FiasPackage> packages, string errorMessage)> GetPackagesAsync();
+    }
+}
---
+++ AuthExample.Fias/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthExample.Fias
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFias(this IServiceCollection services)
+        {
+            services.AddTransient<IFiasService, FiasService>();
+
+            return services;
+        }
+    }
+}
